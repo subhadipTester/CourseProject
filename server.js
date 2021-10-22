af34d66aa8413a17b17fa928f4141a2aeb9ab09e
bosearpan <54657924+bosearpan@users.noreply.github.com>
2021-10-22 11:46:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = 8081;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/*', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello ');
 });
 
 app.listen(PORT, HOST);
